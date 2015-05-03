9e170bfbcb101177805595f45e10e782fbf9293f
evberghe <evberghe@gmail.com>
2015-05-03 19:26:11 +0200

    Made a change on the second repository, which we'll now try and push

--- dssurvey/analyse.R
+++ dssurvey/analyse.R
@@ -1,4 +1,4 @@
-# Adding a change here
+# Adding a change here; and a change on the cloned repository
 # Author: evberghe
 ###############################################################################
 
